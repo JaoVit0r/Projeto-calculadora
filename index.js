18db48d18f94da75a35e6960de12c43ea663f0c9
JaoVitu <joaovitor.nunes.ismart@gmail.com>
2022-03-11 18:54:27 -0300

    Update files

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/* 
-Desenvolva uma calculadora web (HTML/CSS) com as seguintes funcionalidades:
-
-* Soma 
-* Subtração
-* Multiplicação
-* Divisão
-* Resto de uma divisão
-* Raiz quadrada
-* Potenciação
-* Seno
-* Coseno
-* Pi
-
-[ATENÇÃO] A calculadora deve ter um histórico de operações!!!
-    Esse histórico deve ficar armazenado em um array de objetos com os seguintes dados:
-        Valores
-        Operador
-        Resultado
-
-A interface é livre, usem a imaginação!
-*/
 
 function inserir(sinal) {
     let elemento = document.getElementById("resultado").innerHTML;
@@ -35,14 +13,17 @@ function fazerHistorico() {
     document.getElementById("div-historico").innerHTML = historico
 }
 
+function limparHistorico() {
+    document.getElementById("div-historico").innerHTML = ""
+    historico = []
+}
+
 function resultado() {
     resultado = document.getElementById("resultado").innerHTML
     if(resultado)
     {
         var resultado = document.getElementById('resultado').innerHTML = resultado + ' = ' + eval(resultado);
     }
-    
-    // document.getElementById("resultado").innerHTML = historico 
 }
 
 
